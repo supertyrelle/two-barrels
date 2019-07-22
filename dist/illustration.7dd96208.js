acf94146c8a6c43e54ac343faa8b3eb0e4e29186
Tyrelle Phillips <work@supertyrelle.com>
2019-07-21 22:17:51 -0700

    added clickable job cards on front page, transitions and loading animation

--- dist/illustration.7dd96208.js
+++ dist/illustration.7dd96208.js
@@ -6349,7 +6349,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56416" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58289" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/app.js
+++ src/app.js
@@ -1,20 +1,47 @@
 // page transitions!
 import Swup from 'swup';
 const options = {
-    cache: true,
+    // cache: true,
     animateHistoryBrowsing: true,
     containers: ["#swup"]
 
 };
 const swup = new Swup(options);
 
-// tilt properties!
-$('.card').tilt({
-    glare: true,
-    maxGlare: 0.08,
-    disableAxis: X,
-    perspective: 2000
-})
+import AOS from 'aos';
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 0, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+// // tilt properties!
+// $('.card').tilt({
+//     glare: true,
+//     maxGlare: 0.08,
+//     disableAxis: X,
+//     perspective: 2000
+// })
 
 // document.querySelectorAll('header a').forEach((elem) => {
 
